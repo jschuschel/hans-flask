79c8b400acd8ef19dba00eaac925748a3a44a730
slipvyne <jacobschuschel@gmail.com>
2015-05-28 18:16:38 -0700

    initial commit

---
+++ run.py
@@ -0,0 +1,10 @@
+'''
+Written by Jacob Schuschel
+Adapted from the tutorial featured here:
+http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
+'''
+
+
+#!flask/bin/python
+from app import app
+app.run(debug=True)
