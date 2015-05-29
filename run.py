fc476a0ea4ab3995fedfdd2d43abe25f174213d2
slipvyne <jacobschuschel@gmail.com>
2015-05-28 21:35:20 -0700

    adding todos

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ Adapted from the tutorial featured here:
 http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
 '''
 
+#todo: http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
+#todo: http://www.sqlalchemy.org/
+#todo: http://en.wikipedia.org/wiki/Object-relational_mapping
 
 #!flask/bin/python
 from app import app
