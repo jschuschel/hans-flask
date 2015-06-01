0efea532a0ba2ac813bae32c2b2896e299c6d949
slipvyne <jacobschuschel@gmail.com>
2015-05-31 22:06:29 -0700

    (4) - Database done. http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
 __author__ = 'slipvyne'
+import os
+
+#WTForms config
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+#openID config
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+#SQLALCHEMY config
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
